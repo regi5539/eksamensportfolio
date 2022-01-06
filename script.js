639397a3a3a28914daf008ec3819e16b44b7441d
Regitze Reimer Rønneberg <regi5539@stud.kea.dk>
2022-01-06 18:14:25 +0100

    gået tilbage nu virker den første knap på port

--- script.js
+++ script.js
@@ -36,25 +36,23 @@ function MenuClick() {
 const btn_port = document.querySelector(".btn_port");
 const indhold = document.querySelector(".opg_beskrivelse");
 
-// Lav en function, der hedder toggleMenu()
+//function
 function toggleOpg() {
   console.log("toggleOpg");
-  // 1. Toggle en klasse på nav vha. classList.toggle
-  // 2. Toggle en klasse, der hedder "shown"
+  // Her har jeg lavet en en toggle med class fold_ud som er stylingen af tekstboks når man trykker på knappen
   indhold.classList.toggle("fold_ud");
-  // 1. Lav en variabel, der hedder menuShown
-  // 2. Den skal være lig med, om nav-variablen indeholder klassen "shown" vha. classList.contains("")
+  btn_port.classList.toggle("btn_pushed");
+  // Dette er til hvis jeg vælger at have et lille plus eller hvis jeg ønsker at overskrive tekst når den er foldet ud
   let opgShown = indhold.classList.contains("fold_ud");
-  // 1. Lav en if/else sætning => if (...) {...} else {...}
-  // 2. Spørg om menu i if-sætningen => if (menu)
-  if (opgShown) {
-    // hvis nav har klassen "shown", sæt da btn.textContent til "Luk"
-    btn_port.textContent = "Luk";
-  } else {
-    // hvis IKKE nav har klassen "shown", sæt da btn.textContent til "Menu"
-    btn_port.textContent = "Menu";
-  }
+  // if/ else til overskrivning hvis ønsket
+  //   if (opgShown) {
+  //     // hvis class opg.beskrivelse(const indhold) har class fold_ud, sker if
+  //     btn_port.textContent = "Luk";
+  //   } else {
+  //     // hvis IKKE class opg.beskrivelse(const indhold) har class fold_ud, sker else
+  //     btn_port.textContent = "Menu";
+  //   }
 }
 
-/* Tilføj et klik-event til btn, der sætter toggleMenu-funktionen i gang */
+/* click event som sætter function i gang  */
 btn_port.addEventListener("click", toggleOpg);
