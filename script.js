b90b5212e7e6a44cea3827e88ed9ccbe5eb0eab6
Regitze Reimer Rønneberg <regi5539@stud.kea.dk>
2022-01-06 14:22:52 +0100

    portfolio, knap er stylet men virker ikke

--- script.js
+++ script.js
@@ -24,9 +24,37 @@
 // /* Tilføj et klik-event til btn, der sætter toggleMenu-funktionen i gang */
 // btn.addEventListener("click", toggleMenu);
 
-// Ny burgermenu
+// ---------------------Ny burgermenu---------------------------
 function MenuClick() {
   document.getElementById("menu_streger").classList.toggle("click");
   document.getElementById("nav").classList.toggle("click");
   document.getElementById("menu-bg").classList.toggle("click-bg");
 }
+
+// ----------------------portfolio------------------------
+
+const btn_port = document.querySelector(".btn_port");
+const indhold = document.querySelector("opg_beskrivelse");
+
+// Lav en function, der hedder toggleMenu()
+function toggleOpg() {
+  console.log("toggleOpg");
+  // 1. Toggle en klasse på nav vha. classList.toggle
+  // 2. Toggle en klasse, der hedder "shown"
+  indhold.classList.toggle("opg_beskrivelse");
+  // 1. Lav en variabel, der hedder menuShown
+  // 2. Den skal være lig med, om nav-variablen indeholder klassen "shown" vha. classList.contains("")
+  let opgShown = classList.contains("opg_beskrivelse");
+  // 1. Lav en if/else sætning => if (...) {...} else {...}
+  // 2. Spørg om menu i if-sætningen => if (menu)
+  if (opgShown) {
+    // hvis nav har klassen "shown", sæt da btn.textContent til "Luk"
+    btn.textContent = "Luk";
+  } else {
+    // hvis IKKE nav har klassen "shown", sæt da btn.textContent til "Menu"
+    btn.textContent = "Menu";
+  }
+}
+
+/* Tilføj et klik-event til btn, der sætter toggleMenu-funktionen i gang */
+btn.addEventListener("click", toggleMenu);
